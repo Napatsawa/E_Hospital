d1f0e5eaa3e315263e791c0620d18a2850032aa1
Napatsawan <52496355+Napatsawa@users.noreply.github.com>
2025-07-23 18:19:01 +0700

    Update search.js

--- search.js
+++ search.js
@@ -16,7 +16,7 @@ const hnDatabase = {
     status: "ผู้ป่วยพิเศษ",
     note: "เวลาเห็นเขาทำร้ายตังเองอย่าเรียกหมอ",
     bio: "กุลเชอร์เคยเป็นผู้ป่วยมาก่อน และรู้ทุกอย่างในโรงพยาบาลแห่งนี้",
-        images: [
+    images: [
       "https://firebasestorage.googleapis.com/v0/b/myfairytale-v9mv.appspot.com/o/%E0%B8%81%E0%B8%B8%E0%B8%A5%E0%B9%80%E0%B8%8A%E0%B8%AD%E0%B8%A3%E0%B9%8Cpro.png?alt=media&token=6ba89915-1117-4d59-9d68-99ad5eb80f2d",
       "https://firebasestorage.googleapis.com/v0/b/myfairytale-v9mv.appspot.com/o/%E0%B8%81%E0%B8%B8%E0%B8%A5%E0%B9%80%E0%B8%8A%E0%B8%AD%E0%B8%A3%E0%B9%8C%E0%B8%88%E0%B8%A3%E0%B8%B4%E0%B8%87%E0%B8%88%E0%B8%B1%E0%B8%87pro.png?alt=media&token=9deb5cba-114d-46f1-a49b-d3907e74e537",
       "https://firebasestorage.googleapis.com/v0/b/myfairytale-v9mv.appspot.com/o/%E0%B8%81%E0%B8%B8%E0%B8%A5%E0%B9%80%E0%B8%8A%E0%B8%AD%E0%B8%A3%E0%B9%8C%E0%B8%AA%E0%B8%99%E0%B9%83%E0%B8%88pro.png?alt=media&token=0828baf5-af4e-4059-96a1-452978623188",
